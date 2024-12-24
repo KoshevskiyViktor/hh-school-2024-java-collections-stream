6078469e5b5fd52cc27be9a6a5849b0a94254c9b
KoshevskiyViktor <koshevskiyviktorbsu@gmail.com>
2024-12-24 20:41:00 +0300

    _all_tasks

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -3,9 +3,9 @@ package tasks;
 import common.Area;
 import common.Person;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Имеются
@@ -16,9 +16,23 @@ import java.util.Set;
  */
 public class Task6 {
 
+  public static String concatenateNameAndArea(Person person, Area area) {
+    return person.firstName() + " - " + area.getName();
+  }
+
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    Map<Integer, Area> areaNameById = areas.stream()
+        .collect(Collectors.toMap(Area::getId, area -> area));
+
+    return persons.stream()
+        .flatMap(person -> {
+          Set<Integer> areaIds = personAreaIds.getOrDefault(person.id(), Set.of());
+          return areaIds.stream()
+              .map(areaNameById::get)
+              .map(area -> concatenateNameAndArea(person, area));
+        })
+        .collect(Collectors.toSet());
   }
 }
